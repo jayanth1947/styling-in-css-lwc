9486f213390ec8999bdf3030ddba3e02a26f560d
jayanth1947 <jayanthonteru111@gmail.com>
2023-03-10 15:33:28 +0530

    added comment

--- force-app/main/default/lwc/dynamic_css/dynamic_css.js
+++ force-app/main/default/lwc/dynamic_css/dynamic_css.js
@@ -8,6 +8,6 @@ export default class Dynamic_css extends LightningElement {
     }
     // Change the value of the width dynamicaly
     get percentage(){
-        return `width:${this.percent}%`
+        return `width:${this.percent}%` //return the value
     }
 }
